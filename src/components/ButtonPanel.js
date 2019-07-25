421383039159817ec8a2111b23d08c9003fcab38
Zeha Irawan <jangkaringbumi@gmail.com>
2019-07-25 17:47:27 +0700

    refactor using map

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,113 +1,53 @@
 import React from 'react';
 import Button from './Button';
+import firstRow from '../data/firstRow';
+import secondRow from '../data/secondRow';
 
 const ButtonPanel = props => {
   const { clickHandler } = props;
+
+  const thirdRow = ['4', '5', '6', '-'];
+  const fourthRow = ['1', '2', '3', '+'];
+  const fifthRow = ['0', '.', '='];
+
+  const trymap = param =>
+    param.map(e => (
+      <Button
+        names={e}
+        key={e}
+        color='grey'
+        wide={false}
+        clickHandler={clickHandler}
+      />
+    ));
+
+  const trymap2 = param => {
+    let arr = [];
+
+    Object.keys(param).forEach(function(key) {
+      arr.push(param[key]);
+    });
+    return arr.map(e => (
+      <Button
+        key={e.names}
+        names={e.names}
+        color={e.color}
+        wide={e.wide}
+        clickHandler={clickHandler}
+      />
+    ));
+  };
+
   return (
     <div id='buttonPanel'>
-      <div className='row'>
-        <Button
-          names='AC'
-          color='grey'
-          wide={false}
-          clickHandler={clickHandler}
-        />
-        <Button
-          names='+/-'
-          color='grey'
-          wide={false}
-          clickHandler={ clickHandler}
-        />
-        <Button
-          names='%'
-          color='grey'
-          wide={false}
-          clickHandler={ clickHandler}
-        />
-        <Button names='÷' wide={false} clickHandler={ clickHandler} />
-      </div>
+      <div className='row'>{trymap2(firstRow)}</div>
 
-      <div className='row'>
-        <Button
-          names='7'
-          color='grey'
-          wide={false}
-          clickHandler={ clickHandler}
-        />
-        <Button
-          names='8'
-          color='grey'
-          wide={false}
-          clickHandler={ clickHandler}
-        />
-        <Button
-          names='9'
-          color='grey'
-          wide={false}
-          clickHandler={ clickHandler}
-        />
-        <Button names='X' wide={false} clickHandler={ clickHandler} />
-      </div>
+      <div className='row'>{trymap2(secondRow)}</div>
 
-      <div className='row'>
-        <Button
-          names='4'
-          color='grey'
-          wide={false}
-          clickHandler={ clickHandler}
-        />
-        <Button
-          names='5'
-          color='grey'
-          wide={false}
-          clickHandler={ clickHandler}
-        />
-        <Button
-          names='6'
-          color='grey'
-          wide={false}
-          clickHandler={ clickHandler}
-        />
-        <Button names='-' wide={false} clickHandler={ clickHandler} />
-      </div>
+      <div className='row'>{trymap(thirdRow)}</div>
 
-      <div className='row'>
-        <Button
-          names='1'
-          color='grey'
-          wide={false}
-          clickHandler={ clickHandler}
-        />
-        <Button
-          names='2'
-          color='grey'
-          wide={false}
-          clickHandler={ clickHandler}
-        />
-        <Button
-          names='3'
-          color='grey'
-          wide={false}
-          clickHandler={ clickHandler}
-        />
-        <Button names='+' wide={false} clickHandler={ clickHandler} />
-      </div>
-
-      <div className='row'>
-        <Button names='0' color='grey' wide={true} clickHandler={ clickHandler} />
-        <Button
-          names='.'
-          color='grey'
-          wide={false}
-          clickHandler={ clickHandler}
-        />
-        <Button
-          names='='
-          color='grey'
-          wide={false}
-          clickHandler={ clickHandler}
-        />
-      </div>
+      <div className='row'>{trymap(fourthRow)}</div>
+      <div className='row'>{trymap(fifthRow)}</div>
     </div>
   );
 };
