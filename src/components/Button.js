6e1ef9acbd60c34a6cd1608110af836205a4e97a
Zeha Irawan <jangkaringbumi@gmail.com>
2019-07-26 16:37:51 +0700

    use className instead of inline css

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,26 +2,16 @@ import React from 'react';
 
 const Button = props => {
   const { clickHandler, names, wide, color } = props;
-  let bg;
-
-  color === 'grey'
-    ? (bg = {
-        backgroundColor: 'grey'
-      })
-    : (bg = {
-        backgroundColor: 'tomato'
-      });
-
-  let expand;
-
-  wide ? (expand = { width: '50%' }) : (expand = { width: '25%' });
 
   return (
     <div
       onClick={() => clickHandler(names)}
       value={names}
-      className='button'
-      style={{ ...bg, ...expand }}
+      className={
+        'button ' +
+        (color ? 'grey ' : 'tomato ') +
+        (wide ? 'fifty' : 'twentyfive')
+      }
     >
       {names}
     </div>
