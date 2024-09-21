75bd241a27f837245ed66cf8ef19284a6b39bd17
Zeha Irawan <zeha@nubela.co>
2024-09-21 17:09:38 +0800

    form

---
+++ src/components/Conflict.js
@@ -0,0 +1,14 @@
+const Conflict = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" name="name" />
+        <input type="email" name="email" />
+        <input type="password" name="password" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+};
+
+export default Conflict;
